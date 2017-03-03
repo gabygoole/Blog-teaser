86655888062704fe92cbd041ac62130615a528a6
gaby <gabygoole@gmail.com>
2017-03-04 00:26:25 +0800

    RWD footer

---
+++ js/gscript.js
@@ -0,0 +1,47 @@
+/**
+ * Created by Gaby on 3/3/2017.
+ */
+function displayItems1() {
+    var uList = document.getElementById("1");
+    if(uList.style.display == "none") {
+        uList.style.display = "block";
+    } else {
+        uList.style.display = "none";
+    }
+}
+
+function displayItems2() {
+    var uList = document.getElementById("2");
+    if(uList.style.display == "none") {
+        uList.style.display = "block";
+    } else {
+        uList.style.display = "none";
+    }
+}
+
+function displayItems3() {
+    var uList = document.getElementById("2");
+    if(uList.style.display == "none") {
+        uList.style.display = "block";
+    } else {
+        uList.style.display = "none";
+    }
+}
+
+function displayItems4() {
+    var uList = document.getElementById("2");
+    if(uList.style.display == "none") {
+        uList.style.display = "block";
+    } else {
+        uList.style.display = "none";
+    }
+}
+
+function displayItems5() {
+    var uList = document.getElementById("2");
+    if(uList.style.display == "none") {
+        uList.style.display = "block";
+    } else {
+        uList.style.display = "none";
+    }
+}
