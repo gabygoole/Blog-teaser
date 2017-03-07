cda7523d4fdfc6b1be50c8caee7a81233148a3e7
gaby <gabygoole@gmail.com>
2017-03-07 23:43:39 +0800

    Refactor gscript.js and fix index.html footer bug

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ## Try blog
+Since there is no block scope in JavaScript - only function scope - by wrapping the function creation in a new function, you ensure that the value of "i" remains as you intended.
--- js/gscript.js
+++ js/gscript.js
@@ -1,47 +1,44 @@
 /**
  * Created by Gaby on 3/3/2017.
  */
-function displayItems1() {
-    var uList = document.getElementById("1");
-    if(uList.style.display == "none") {
-        uList.style.display = "block";
-    } else {
-        uList.style.display = "none";
+window.onload = prepareCollapse;
+
+function prepareCollapse() {
+    if(window.innerWidth > 414){
+        return false;
     }
+    if(!document.getElementsByClassName) {
+        return false;
+    }
+    if(!document.getElementsByClassName("column")) {
+        return false;
+    }
+    if(!document.getElementsByClassName("title")) {
+        return false;
+    }
+
+    var fiveColumns = document.getElementsByClassName("column");
+    for(var i = 0; i < fiveColumns.length; i++) {
+        var currenColumn = fiveColumns[i];
+        var aTag = currenColumn.getElementsByClassName("title")[0];
+        var ulTag = currenColumn.getElementsByClassName("items")[0];
+        var style = getComputedStyle(ulTag);
+        var ulTagDisplay = style.getPropertyValue("display");
+        aTag.onclick = funcCreation(ulTagDisplay,ulTag);
+    }
+
 }
 
-function displayItems2() {
-    var uList = document.getElementById("2");
-    if(uList.style.display == "none") {
-        uList.style.display = "block";
-    } else {
-        uList.style.display = "none";
+function funcCreation(ulTagDisplay,ulTag) {
+    return function () {
+        if (ulTagDisplay == "none"){
+            ulTag.style.display = "block";
+            ulTagDisplay = "block";
+        } else {
+            ulTag.style.display = "none";
+            ulTagDisplay = "none";//这个地方可以改变第27行的ulTagDisplay
+        }
+        return false;
     }
-}
 
-function displayItems3() {
-    var uList = document.getElementById("3");
-    if(uList.style.display == "none") {
-        uList.style.display = "block";
-    } else {
-        uList.style.display = "none";
-    }
-}
-
-function displayItems4() {
-    var uList = document.getElementById("4");
-    if(uList.style.display == "none") {
-        uList.style.display = "block";
-    } else {
-        uList.style.display = "none";
-    }
-}
-
-function displayItems5() {
-    var uList = document.getElementById("5");
-    if(uList.style.display == "none") {
-        uList.style.display = "block";
-    } else {
-        uList.style.display = "none";
-    }
 }
