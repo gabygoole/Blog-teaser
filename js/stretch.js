1aff5c5fdc2f8fbaaff74fefcbbe1f0f88cea85c
gaby <gabygoole@gmail.com>
2017-03-15 23:28:11 +0800

    Add personal blog page

---
+++ js/stretch.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Gaby on 3/15/2017.
+ */
+$(document).ready(function () {
+    $('.g-navbar').click(function () {
+        $('header div.menu').toggleClass('shownav');
+    });
+});
